33b3feaec9146cbc281983dc2df4c73179526ce0
renanrfds <renan.rosa37@gmail.com>
2021-05-16 18:47:30 -0300

    Incluindo Validador de CPF

--- logica.js
+++ logica.js
@@ -70,3 +70,66 @@ function pesquisacep(valor) {
     }
 };
 
+//Validação de CPF
+function is_cpf(c) {
+
+    if ((c = c.replace(/[^\d]/g, "")).length != 11)
+        return false
+
+    if ([c == '00000000000',
+                '11111111111'])
+        return false;
+
+    var r;
+    var s = 0;
+
+    for (i = 1; i <= 9; i++)
+        s = s + parseInt(c[i - 1]) * (11 - i);
+
+    r = (s * 10) % 11;
+
+    if ((r == 10) || (r == 11))
+        r = 0;
+
+    if (r != parseInt(c[9]))
+        return false;
+
+    s = 0;
+
+    for (i = 1; i <= 10; i++)
+        s = s + parseInt(c[i - 1]) * (12 - i);
+
+    r = (s * 10) % 11;
+
+    if ((r == 10) || (r == 11))
+        r = 0;
+
+    if (r != parseInt(c[10]))
+        return false;
+
+    return true;
+}
+//Macara do cpf
+
+function fMascara(objeto, mascara) {
+    obj = objeto
+    masc = mascara
+    setTimeout("fMascaraEx()", 1)
+}
+
+function fMascaraEx() {
+    obj.value = masc(obj.value)
+}
+
+function mCPF(cpf) {
+    cpf = cpf.replace(/\D/g, "")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2")
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    return cpf
+}
+
+cpfChecar = function (el) {
+    document.getElementById('cpfResponse').innerHTML = is_cpf(el.value) ? '<span style="color:green">válido</span>' : '<span style="color:red">inválido</span>';
+    if (el.value == '') document.getElementById('cpfResponse').innerHTML = '';
+}
